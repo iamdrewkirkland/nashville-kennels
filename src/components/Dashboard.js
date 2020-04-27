4b9daac757b540be5bf37d8e4da4638fc148c3bc
iamdrewkirkland <drew.kirkland@gmail.com>
2020-04-27 15:41:28 -0400

    Added functionality to render current view based on which list the user clicks on.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Kennel.css";
 import "./Layout.css";
 import "./employees/Employees.css";
@@ -7,7 +7,6 @@ import { LocationProvider } from "./locations/LocationProvider";
 import LocationList from "./locations/LocationList";
 import "./animal/Animal.css";
 import { AnimalProvider } from "./animal/animalProvider";
-import AnimalList from "./animal/AnimalList";
 import "./customers/Customers.css";
 import { CustomerProvider } from "./customers/customerProvider";
 import CustomerList from "./customers/CustomerList";
@@ -16,34 +15,88 @@ import EmployeeList from "./employees/EmployeeList";
 import { SearchBar } from "./search/SearchBar";
 import { SearchResults } from "./search/SearchResults";
 
+
 export const Dashboard = () => {
-  const [searchTerms, setTerms] = useState("");
+  const [searchTerms, setTerms] = useState(null);
+  const [activeList, setActiveList] = useState("locations");
+  const [components, setComponents] = useState();
+
+  // Components needed to display locations
+  const showLocations = () => (
+    <LocationProvider>
+      <LocationList />
+    </LocationProvider>
+  );
+
+  // Components needed to display customers
+  const showCustomers = () => (
+    <CustomerProvider>
+      <CustomerList />
+    </CustomerProvider>
+  );
+
+  // Components needed to display employee
+  const showEmployees = () => (
+    <EmployeeProvider>
+      <LocationProvider>
+        <EmployeeList />
+      </LocationProvider>
+    </EmployeeProvider>
+  );
+
+  /*
+      This effect hook determines which list is shown
+      based on the state of the `activeList` variable.
+  */
+  useEffect(() => {
+    if (activeList === "customers") {
+      setComponents(showCustomers);
+    } else if (activeList === "locations") {
+      setComponents(showLocations);
+    } else if (activeList === "employees") {
+      setComponents(showEmployees);
+    }
+  }, [activeList]);
 
   return (
-    <>
-    <h2>Nashville Kennels</h2>
-                <small>Loving care when you're not there.</small>
     <div className="mainContainer">
-      <AnimalProvider>
-        <CustomerProvider>
-          <EmployeeProvider>
+      <div className="searchContainer">
+        <AnimalProvider>
+          <CustomerProvider>
             <LocationProvider>
-              <div className="searchContainer">
-                <SearchBar setTerms={setTerms} />
-                <SearchResults searchTerms={searchTerms} />
-              </div>
-              <div className="dataContainer">
-                
-                <LocationList />
-                <AnimalList />
-                <CustomerList />
-                <EmployeeList />
-              </div>
+              <SearchBar setTerms={setTerms} />
+              <SearchResults searchTerms={searchTerms} />
             </LocationProvider>
-          </EmployeeProvider>
-        </CustomerProvider>
-      </AnimalProvider>
+          </CustomerProvider>
+        </AnimalProvider>
+      </div>
+      <div className="dataContainer">
+        <h1>Nashville Kennels</h1>
+        <small>Loving care when you're not there.</small>
+        <div className="listContainer">
+          <div className="links">
+            <div
+              className="fakeLink href"
+              onClick={() => setActiveList("locations")}
+            >
+              Locations
+            </div>
+            <div
+              className="fakeLink href"
+              onClick={() => setActiveList("customers")}
+            >
+              Customers
+            </div>
+            <div
+              className="fakeLink href"
+              onClick={() => setActiveList("employees")}
+            >
+              Employees
+            </div>
+          </div>
+          <div className="listDisplay">{components}</div>
+        </div>
+      </div>
     </div>
-    </>
   );
 };
